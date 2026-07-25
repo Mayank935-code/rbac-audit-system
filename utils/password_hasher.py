1e23c8d883dae0e9aba0f2948cc240b1a5596032
Mayank935-code <148620862+Mayank935-code@users.noreply.github.com>
2026-07-25 20:10:40 +0530

    Delete utils directory

--- utils/password_hasher.py
+++
@@ -1,9 +0,0 @@
-# utils/password_hasher.py
-
-import hashlib
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def check_password(input_password, stored_hash):
-    return hash_password(input_password) == stored_hash
